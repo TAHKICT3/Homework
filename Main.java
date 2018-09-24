49f4fc0372c334f85af269121fa0a5c55ae6a869
TAHKICT3 <43368370+TAHKICT3@users.noreply.github.com>
2018-09-24 21:11:33 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,16 +1,33 @@
-package sample;
+package HW1;
+import java.io.IOException;
+public class Main {
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
+    public static void main(String[] args) {
+	int a, b, c, D;
+	double d,e;
+	try{
+	    a = System.in.read();
+        b = System.in.read();
+        c = System.in.read();
+        a=a-48;
+        b = b-48;
+        c=c-48;
+        D=(b*b)-(4*a*c);
+        if (D > 0) {
+            d = (-b + Math.sqrt(D)) / (2 * a);
+            e = (-b - Math.sqrt(D)) / (2 * a);
+            System.out.print(d+ ";" +e);
+        }
+        else if (D==0){
+            d = -b / (2 * a);
+            System.out.print(d);
+        }
+        else
+            System.out.print("Уравнение корней не имеет.");
 
-public class Main{
+    }
+    catch (IOException ex){
 
-
-    public static void main(String[] args)
-    {
-        System.out.println("Hello world!");
+    }
     }
 }
