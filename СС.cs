1fcf77876b4bf9e8b98be11fe023be224de7c421
TAHKICT3 <43368370+TAHKICT3@users.noreply.github.com>
2019-05-18 18:03:54 +0300

    Add files via upload

---
+++ СС.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class СС : MonoBehaviour {
+
+    GameObject []cams;
+    int d;
+	void Start () {
+        d = 0;
+        cams = GameObject.FindGameObjectsWithTag("MainCamera");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.M))
+            Setactive("MainCamera");
+        if (Input.GetKeyDown(KeyCode.N))
+            Setactive("Camera");
+    }
+    private void Setactive(string name)
+    {
+        d++;
+        var o = cams.First(c => c.name == name);
+        o.GetComponent<Camera>().depth = d;
+    }
+}
